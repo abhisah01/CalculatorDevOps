827777d36969dc76c5a42ae404affedcef281556
Abhijeet Kumar <abhijeet.kumar@iiitb.org>
2021-03-17 21:57:41 +0530

    Added log files

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,29 +1,43 @@
 import java.util.Scanner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Calculator {
+    private static final Logger log = LogManager.getLogger(Calculator.class); // Logger class object to store logs
+
     static double squareRoot(double n) { // function to find the square root of a number
+        log.info("[Square root function] - " + n);
+        log.info("[Result] - " + Math.sqrt(n));
         return Math.sqrt(n);
     }
 
     static double factorial(double n) { // function to find the factorial of a number
+        log.info("[Factorial function] - " + n);
         try { // for handling the case when we pass a negative number as input
             if (n < 0)
                 throw new ArithmeticException(); // throwing the Arithmetic Exception
         } catch (ArithmeticException e) { // catching the exception
+            log.info("[Result] - " + Double.NaN);
             return Double.NaN; // returning NaN
         }
 
         double answer = 1;
         for(int i = 2; i <= n; i++)
             answer *= i;
+
+        log.info("[Result] - " + answer);
         return answer;
     }
 
     static double logarithm(double n) { // function to find the log base e value of a number
+        log.info("[Logarithm function] - " + n);
+        log.info("[Result] - " + Math.log(n));
         return Math.log(n);
     }
 
     static double power(double X, double b) { // function to find X power b
+        log.info("[Power function] - " + X + " power " + b);
+        log.info("[Result] - " + Math.pow(X, b));
         return Math.pow(X, b);
     }
 
